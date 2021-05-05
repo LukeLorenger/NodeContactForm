7254b9782663e80a31e04c231b4395bf2679ca4f
Luke Lorenger <dirtydeadtv@gmail.com>
2021-05-05 10:25:00 -0400

    nodemailer sending emails

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 const express = require('express');
-const app = express()
+const app = express();
+
+const nodemailer = require("nodemailer");
 
 const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.static('public'));
+app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/public/contactform.html')
+})
+
+app.post('/', (req, res)=>{
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'your gmail here',
+            pass: 'your password here'
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'your gmail here',
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        }else{
+            console.log('Email sent' + info.response)
+            res.send('success')
+        }
+    })
 })
 
 app.listen(PORT, ()=>{
